4fa56e637530f0d445c1fa2cad25cf09a23a1813
channiemills <cmiller20@gmail.com>
2018-08-19 08:38:47 -0500

    modified utilities to clean lift name and make component variable since lift titles and header are different in metcon reports

--- utilities.py
+++ utilities.py
@@ -9,7 +9,7 @@ import openpyxl
 
 # TODO probably want to make download dir a variable
 filenames = glob.glob(r'C:\Users\cmill\Downloads\PerformanceResultsWeightlifting*.xlsx')
-
+metcons = glob.glob(r'C:\Users\cmill\Downloads\PerformanceResultsMetcon*.xlsx')
 
 def file_rename(filenames, cycle):
     """
@@ -19,10 +19,20 @@ def file_rename(filenames, cycle):
     for f in filenames:
         wb = openpyxl.load_workbook(f)
         sheet1 = wb.get_sheet_by_name("Sheet1")
-        lift = sheet1["L2"].value
+        header = list(sheet1.rows)[0]
+        comp = [col.column for col in header if col.value == 'Component'][0]
+        liftname = sheet1[comp+'2'].value
+        lift = clean_name(liftname)
         wb.close()
         dst = cycle + '_' + lift + '.xlsx' # will likely need to prepend dir to keep from being moved to wd
         os.rename(f, dst)
 
 
-file_rename(filenames, 'summer18cycle')
+def clean_name(liftname):
+    f = liftname.rsplit(" (CrossFit Commitment)")[0]
+    return ''.join(l for l in f if l.isalnum())
+
+
+#file_rename(filenames, 'summer18cycle')
+
+file_rename(metcons, 'summer18cycle')
