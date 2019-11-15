73477f22dea4be06bfd96fb5ac7b15a6a473b65b
channiemills <cmiller20@gmail.com>
2019-11-14 21:26:55 -0600

    update conf vars

--- conf_vars.py
+++ conf_vars.py
@@ -2,8 +2,10 @@
 Configuration variables needed for processing and utilities.
 """
 
-CYCLE = 'testing'
-TESTING_START = '10/08/2018'
-TESTING_END = '10/21/2018'
+CYCLE = 'cycle_04_2019'
+ATTENDANCE_START = '07/29/2019'
+ATTENDANCE_END = '09/22/2019'
+TESTING_START = '09/23/2019'
+TESTING_END = '10/06/2019'
 DOWNLOADS_DIR = f'{CYCLE}\\{CYCLE}_downloads'
 RESULTS_DIR = f'{CYCLE}\\{CYCLE}_results'
